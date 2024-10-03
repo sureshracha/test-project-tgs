7002a6e612e1fb1ff77f2dfc4bd08887ab7f4de6
sureshracha <31437248+sureshracha@users.noreply.github.com>
2024-10-03 15:50:17 +0530

    Update tests.ts

--- examples/tests.ts
+++ examples/tests.ts
@@ -4,7 +4,7 @@ import { createLogger } from "winston";
 import logger, { options } from "../test-automation-wrappers/lib/logger";
 import testContext from '../test-automation-wrappers/lib/testContext';
 import customAssert from '../test-automation-wrappers/lib/assert';
-import * as fileUtils from '../test-automation-wrappers/utils/file.utils'
+import * as fileUtils from '../test-automation-wrappers/utils/file.utils';
 
 // Tips :to run only one test  append '.only' after describe
 describe('A first sample Playwright test', () => {
@@ -35,22 +35,24 @@ describe('A first sample Playwright test', () => {
 
 
     it('Search Playwright documentation from Goggle', async () => {
+        let year = '2024'
         let partyName = new UiElement('#link4', { description: 'party Name' });
         await gotoUrl('https://csis.tshc.gov.in/');
+        await playwright.page.waitForTimeout(5000);
         await partyName.click();
         await new UiElement('#both',{description: 'Radio both'}).click();
         await new UiElement('#pet',{description: 'party name both'}).setValue('TSPSC');
-        await new UiElement('#pyear',{description: 'party year both'}).setValue('2024');
+        await new UiElement('#pyear',{description: 'party year both'}).setValue(year);
        
         console.log('yes');
         
         await new UiElement('#searchfour',{description: 'Search button'}).click();
         // await new UiElement('#pcaptcha',{description: 'party captcha '}).setValue('2024');
-        await playwright.page.waitForTimeout(5000);
+        await playwright.page.waitForTimeout(10000);
         console.log('yes');
         let tabl = new UiTable('#inftable');
        let rows =  await tabl.getRowsLength();
-         
+       let json = JSON.parse('[]');
         for(let row = 2; row<rows; row++){
             let sts = await new UiTable('#inftable').getCellData(row,3);
             let r=row;
@@ -58,7 +60,7 @@ describe('A first sample Playwright test', () => {
                 
                 let linkName = await new UiTable('#inftable').getCellData(r,0);
                await (await new UiTable('#inftable').getRow(r)).clickLink(linkName,{force:true}) 
-                  await playwright.page.waitForTimeout(5000);
+                  await playwright.page.waitForTimeout(10000);
                  const b =  await playwright.page.frameLocator(`//iframe`);
 
    
@@ -68,8 +70,8 @@ describe('A first sample Playwright test', () => {
                let cnrNumber = await b.locator('//*[@id="inftable"]/tbody/tr[3]/td[2]').innerText(); 
                let registerDate = await b.locator('//*[@id="inftable"]/tbody/tr[7]/td[4]').innerText();  
 
-                let json = JSON.parse('[]');
-                let row = JSON.parse(`{ "FiledName": ${linkName},"" : ${sts}} , "":${filingDate}, "ListingDate":${listingDate}, "purpose": ${purpose},"CRN": ${cnrNumber},"RegistrationDate": ${registerDate} }`)
+               
+                let row ={ "FiledName": linkName,"Status" :sts , "FileingDate":filingDate, "ListingDate":listingDate, "purpose": purpose,"CRN":cnrNumber,"RegistrationDate": registerDate }
                 // json.FiledName = linkName;
                 // json.Status = sts;
                 // json.FileingDate = filingDate;
@@ -77,15 +79,17 @@ describe('A first sample Playwright test', () => {
                 // json.Purspose = purpose;
                 // json.CNRNumber = cnrNumber;
                 // json.RegisterDate = registerDate;
-                json.push(row);
+                json.push( row);
                 console.log(JSON.stringify(json));
-                
+                await playwright.page.locator('//html/body/div[2]/div[2]').click();
+                await playwright.page.waitForTimeout(10000);
                 console.log('yes');
             }
 
         } 
        
 
+        await fileUtils.writeJsonData(`D:/padma/cases_${year}.json`,json);
         return true;
     })
 
